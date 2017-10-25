001a6da0c8b6ccab26a3fe80f570dc7faddf6752
zpwang <wzp123#@!>
2017-10-25 13:48:17 +0800

    init mock db

--- mockit-db/src/main/java/com/kotall/mock/db/MockDb.java
+++ mockit-db/src/main/java/com/kotall/mock/db/MockDb.java
@@ -1,5 +1,12 @@
 package com.kotall.mock.db;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MockDb {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MockDb.class);
+
+
 
 }
---
+++ mockit-db/src/test/resources/test-data.sql
@@ -0,0 +1,3 @@
+-- MOCK_TEST
+insert into `MOCK_TEST` (`ID`, `USER_NAME`, `USER_PWD`) values('1','TEST1','123456');
+insert into `MOCK_TEST` (`ID`, `USER_NAME`, `USER_PWD`) values('2','TEST2','123456');
