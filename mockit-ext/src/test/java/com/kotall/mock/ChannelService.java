5a5efd652fcd996c32876c23096a244396d55f23
aracwong <aracwong@163.com>
2017-10-31 22:17:25 +0800

    jmockit test

---
+++ mockit-ext/src/test/java/com/kotall/mock/ChannelService.java
@@ -0,0 +1,12 @@
+package com.kotall.mock;
+
+/**
+ * @author: aracwong
+ * @email: aracwong@163.com
+ * @datetime: 2017/10/31 0031 下午 10:04
+ * @version: 1.0.0
+ */
+public interface ChannelService {
+
+    String requestBank(String msg);
+}
--- mockit-ext/src/test/java/com/kotall/mock/JMockitTest.java
+++ mockit-ext/src/test/java/com/kotall/mock/JMockitTest.java
@@ -4,8 +4,10 @@ import mockit.Mock;
 import mockit.MockUp;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
+ *  http://blog.csdn.net/fei33423/article/details/46653501
  * @author: aracwong
  * @email: aracwong@163.com
  * @datetime: 2017/10/30 0030 下午 9:02
